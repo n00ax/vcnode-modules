42fbbd02e4d09896736dee9f1e8e05b701981340
Noah Whiteis <nwhiteis@Noahs-MacBook-Pro.local>
2017-04-13 02:05:49 -0500

    blank:)

---
+++ remoteBaseDescriptor.js
@@ -0,0 +1,14 @@
+module.exports = {
+    thisModule : {
+        moduleName : "Base Descriptor",
+        moduleVersion : "1.0.0",
+        moduleFiles : [
+		       ],
+        requiredModules : [
+{
+    modulePath : "testModule",
+    moduleDescriptor : "testModuleDescriptor.js"
+}
+        ]
+    }
+}
---
+++ testModule/testModuleDescriptor.js
@@ -0,0 +1,9 @@
+module.exports = {
+    moduleName : "Test Module",
+    moduleVersion : "1.0.0",
+    moduleFiles : {
+	"testModule.js"
+    },
+    requiredModules : {
+    }
+}
