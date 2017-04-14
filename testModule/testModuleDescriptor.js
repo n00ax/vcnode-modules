ec7ad55f8fd0be3f045552884b70868e7cae1f95
Noah Whiteis <nwhiteis@saltedgrove.net>
2017-04-13 20:10:22 -0500

    Update testModuleDescriptor.js

--- testModule/testModuleDescriptor.js
+++ testModule/testModuleDescriptor.js
@@ -3,7 +3,7 @@ module.exports = {
     	moduleName : "Test Module",
     	moduleVersion : "1.0.0",
     	moduleFiles : [
-		"testModule.js"
+		"testModule/testModule.js"
     	],
     	requiredModules : [
     	]
